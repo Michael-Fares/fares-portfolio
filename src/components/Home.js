a590cb51b304640db1bfe3cc6248bdd9bc1b6def
Michael-Fares <mjfares@gmail.com>
2021-11-27 10:11:29 -0600

    added image

--- src/components/Home.js
+++ src/components/Home.js
@@ -35,6 +35,9 @@ const Home = () => {
     <>
     <main className="flex-column">
       {title && <section className="title">
+        <div className="img-container">
+
+        </div>
         <h1>Michael Fares</h1>
         <h3>Web Developer</h3>
         <div className="icon-row">
@@ -44,9 +47,15 @@ const Home = () => {
         <a href="https://github.com/Michael-Fares" target="blank">
         <FontAwesomeIcon className="icon" icon={faGithubSquare} size="2x"/>
         </a>
+        <a href="https://www.instagram.com/therealmjfares/" target="blank">
         <FontAwesomeIcon className="icon" icon={faInstagramSquare} size="2x"/>
+        </a>
+        <a href="https://www.youtube.com/channel/UCuWqeRDlqAj-MFiFrf3xyqg" target="blank">
         <FontAwesomeIcon className="icon" icon={faYoutubeSquare} size="2x"/>
+        </a>
+        <a href="mailto:mjfares@gmail.com">
         <FontAwesomeIcon className="icon" icon={faEnvelopeSquare} size="2x"/>
+        </a>
        
 
         </div>
