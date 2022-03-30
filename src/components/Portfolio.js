320b155efde17f4829e9127cc4fd1e326cc14193
Michael Fares <mjfares@gmail.com>
2022-03-30 17:16:32 -0500

    added Zollege project to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,11 +8,23 @@ const [repos, setRepos] = useState([])
 const [loading, setLoading] = useState(true)
 
 useEffect(() => {
-
+  const mvpRepo1 = {
+    id: "78219421",
+    name: "landingpage",
+    html_url: "https://github.com/theMVPshop/landingpage",
+    homepage: "https://michael-fares.github.io/landingpage/html/landing.html",
+    topics: ["Bootstrap 5", "CSS", "HTML", "Design implementation"],
+    description: "Pixel perfect implementation of a product landing page design for Zospace Apps at Zollege"
+  }
   axios.get(`https://api.github.com/users/michael-fares/repos?sort=created&direction=desc&per_page=100`)
   .then((res) => {
     const filteredRepos = res.data.filter(repo => repo.homepage && repo.description)
+    
+    // add customised MVP shop first project to beginning of github API fetched repos array
+    filteredRepos.unshift(mvpRepo1)
+    
     setRepos(filteredRepos)
+    console.log("repos", filteredRepos)
     setLoading(false)
   })
 
